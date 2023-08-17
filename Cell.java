81abb3a7a2751eb6a63f46b3add98b2d9f40efe5
Paulameena Shultes <32913103+paulameena@users.noreply.github.com>
2023-08-17 11:28:40 -0400

    building out intiialization of model and code cleanup

--- Cell.java
+++ Cell.java
@@ -4,40 +4,71 @@ import java.lang.String;
 
 
 
-public class Cell extends AgentSQ2Dunstackable<SimpleGrid>{
+public class Cell extends AgentSQ2Dunstackable<Fusion>{
     //cell characteristics
-    static String cellType;
-    double replicationRate;
-    double deathRate;
+     String cellType; //options are p, r, or f
+    
+    //making these cell-type specific (instead of model-specific) so unique phenotypic behaviors can be adopted later
     //double mutationRate; 
-    //double resistanceRate;
+    double divRate;
+    double resistanceRate;
+    double deathRate;
+    
 
     //CONSTRUCTOR NOT NEEDED ACCORDING TO HAL DOCUMENTATION?
-    
+
     // public Cell(String cellType, double replicationRate, double deathRate){
     //     this.cellType = cellType;
     //     this.replicationRate = replicationRate;
     //     this.deathRate = deathRate;
     // }
 
-    public String getCellType() {
+     /*------------------------------------------
+               HOUSEKEEPING FUNCTIONS
+     ------------------------------------------*/
+     public String getCellType() {
         return cellType;
     }
 
-    public double getReplicationRate() {
-        return replicationRate;
+    public void setCellType(String cellType) {
+        this.cellType = cellType;
+    }
+    public double getDivRate() {
+        return this.divRate;
     }
 
     public double getDeathRate() {
-        return deathRate;
+        return this.deathRate;
     }
 
-    public void setReplicationRate(double newReplicationRate) {
-        this.replicationRate = newReplicationRate;
+    public double getResistanceRate() {
+        return this.resistanceRate;
+    }
+
+    public void setDivRate(double newDivRate) {
+        this.divRate = newDivRate;
     }
 
     public void setDeathRate(double newDeathRate) {
         this.deathRate = newDeathRate;
+    }
+
+/*------------------------------------------
+                KEY FUNCTIONS
+     ------------------------------------------*/
+     //NOTE: ADAPTED FROM HAL DOCUMENTATION AND BIRTHDEATH.JAVA FROM HAL DOCS
+
+    public void Step() {
+        if (G.rng.Double() < this.deathRate) {
+            Dispose();
+            return;
+        }
+        if (G.rng.Double() < G.replicationRate) {
+            int nOptions = G.MapEmptyHood(G.VonNeumannHood, Xsq(), Ysq());
+            if(nOptions>0) {
+                G.NewAgentSQ(G.VonNeumannHood[G.rng.Int(nOptions)]).;
+            }
+        }
     }
   
 
--- Fusion.java
+++ Fusion.java
@@ -1,21 +1,26 @@
 // Written by: Paulameena Shultes
 //**ADAPTED FROM HAL DOCUMENTATION AND GITHUB.COM/ESHANKING/HAL_DOSE_RESPONSE.GIT**
 // Date: 08/10/2023
-import HAL.*;
 import HAL.Interfaces.SerializableModel;
-
-import java.util.*;
-import java.io.*;
+import HAL.Util;
+//import java.util.*;
+//import java.io.*;
 import HAL.GridsAndAgents.AgentGrid2D;
 import HAL.Tools.FileIO;
-import HAL.Gui.UIGrid;
+//import HAL.Gui.GridWindow;
+//import HAL.Gui.UIGrid;
+import HAL.Rand;
+//import HAL.GridsAndAgents.PDEGrid2D;
+//import HAL.GridsAndAgents.AgentSQ2Dunstackable;
 
-/*SIMPLE GRID
+
+
+/*Fusion
 
 
 HAL is used to define the grid and the cells, and to set up the simulation parameters.
 The grid is initialized with a set of cells, and the cells are initialized with their characteristics.
-The grid is then passed to the simulation class, where the simulation is run.
+
 
 Note: the way the HAL library is defined; agents are not constructed by you but by the grid. No agent constructors are needed.
  
@@ -51,8 +56,13 @@ public class Fusion extends AgentGrid2D<Cell> implements SerializableModel{
 
     int xDim= 100;
     int yDim = 100;
+    int[] divHood = HAL.Util.VonNeumannHood(false); // 4 neighbors
+    Rand rng=new Rand(); 
+    //double replicationRate = 0.2; // birth probability
+    //double deathRate = 0.01; // death probability
+    //double fusionRate = 0.1; //fusion probability
+    double initResProb = 0.1; // mutation probability for initial seeding
 
-    int[] divHood = Util.VonNeumannHood(false); // 4 neighbors
     //int[] divHood = Util.MooreHood(false); // 8 neighbors-- doesn't make sense for fusion 
     public double initRadius = 10;
     public double initMutProp=0.1;
@@ -64,18 +74,18 @@ public class Fusion extends AgentGrid2D<Cell> implements SerializableModel{
                 CELL PARAMETERS
      ------------------------------------------*/
 
-    double dieProb = 0.1; // death probability
-    double resistanceRate = 0.0001; // mutation rate to resistant from parental/sensitive
-    double timeStep = 1; // time step for simulation
-    
-    //TODO: might want to change this to be a function that samples from a distribution with different centers, to create more variability between the cell types/introduce more heterogeneity
-    double[] divProbs = new double[]{0.1,0.2}; // probability of division for each cell type, first = parent, second = resistant
+    //TODO: might want to change these to be a function that samples from a distribution with different centers, to create more variability between the cell types/introduce more heterogeneity
+    //index = 0 is parental, index = 1 is resistant, index = 2 will be fused
+    double[] dieProbs = new double[]{0.1,0.1}; // death probability
+    double[] resistanceRates = new double[]{0.0001,0}; // mutation rate to resistant from parental/sensitive; if already sensitive, does nothing
+    double[] divProbs = new double[]{0.1,0.2}; // probability of division for each cell type
 
    /*------------------------------------------
                 SIMULATION PARAMETERS
      ------------------------------------------*/
 
     int maxTime = 10000; // max time for simulation
+    double timeStep = 1; // time step for simulation
     public int nReplicates = 1; // number of replicates
 
 /*------------------------------------------
@@ -84,6 +94,54 @@ public class Fusion extends AgentGrid2D<Cell> implements SerializableModel{
 
      FileIO cellCountLogFile = null;
      String cellCountLogFileName = "./data/cellCountLog.csv";
+
+
+
+    /*------------------------------------------
+                MODEL FUNCTIONS
+     ------------------------------------------*/
+     //NOTE: ADAPTED FROM HAL DOCUMENTATION AND BIRTHDEATH.JAVA FROM HAL DOCS
+
+    public void InitializeTumor(double radius, String shape){
+        int[] coords;
+        int nCoords;
+        if (shape.equals("circle")) {
+            coords= Util.CircleHood(true,radius);
+            nCoords= MapHood(coords,xDim/2,yDim/2);
+            }
+        else {
+            coords= Util.RectangleHood(true,(int) radius, (int) radius);
+            nCoords= MapHood(coords,xDim/2,yDim/2);
+            }
+        for (int i = 0; i < nCoords ; i++) {
+                Cell c = NewAgentSQ(coords[i]);
+                if (rng.Double() < initResProb) {
+                    c.cellType = "r";
+                    c.divRate = divProbs[1];
+                    c.deathRate = dieProbs[1];
+                    c.resistanceRate = 0; //already resistant!  
+                } else {
+                    c.cellType = "p";
+                    c.divRate = divProbs[0];
+                    c.deathRate = dieProbs[0];
+                    c.resistanceRate = resistanceRates[0];
+                }
+        }
+    }
+
+    public void Step() {
+        for (Cell c : this) {
+            c.Step();
+        }
+        // CleanAgents(); // not sure what these are for/ why included?
+        // ShuffleAgents(rng);
+    }
+    // public void Draw(GridWindow vis){
+    //     for (int i = 0; i < vis.length; i++) {
+    //         Cell c = GetAgent(i);
+    //         vis.SetPix(i, c == null ? BLACK : c.color);
+    //     }
+    // }
 
 
 
